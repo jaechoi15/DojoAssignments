6f495f7e8b30191b7a91f6f7237258abc727e687
JaeChoi <jchoi625@gmail.com>
2017-11-12 09:09:27 -0500

    added I Do Declare assignemnt

---
+++ WebFundamentals/Javascript/i_do_declare.js
@@ -0,0 +1,28 @@
+/*Numbers*/
+var number1 = 123456789;
+var number2 = -981203;
+var number3 = 23532.543;
+var number4 = -522.55;
+
+/*Strings*/
+var string1 = "Coding Dojo";
+var string2 = "Pancakes and eggs!"
+var string3 = "Thanksgiving is on November 23rd, 2017"
+
+/*Booleans*/
+var boolean1 = True;
+var boolean2 = False;
+
+/*Undefined*/
+var undef = "";
+
+console.log("The first number:", number1);
+console.log("The second number:", number2);
+console.log("The third number:", number3);
+console.log("The fourth number:", number4);
+console.log("The first string:", string1);
+console.log("The second string:", string2);
+console.log("The third string:", string3);
+console.log("The first boolean:", boolean1);
+console.log("The second boolean:", boolean2);
+console.log("This is an undefined variable:", undef);
