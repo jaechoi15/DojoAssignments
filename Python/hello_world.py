f12870ef0cd990054588e4d0e34e8c08e65a6d86
JaeChoi <jchoi625@gmail.com>
2017-11-26 22:10:22 -0500

    added stringsandlists assignment

---
+++ Python/hello_world.py
@@ -0,0 +1,38 @@
+# x = "Hello Python"
+# print x
+# y = 42
+# print y
+
+# first_name = "Zen"
+# last_name = "Coder"
+# print "My name is {} {}".format(first_name, last_name)
+
+# hw = "hello %s" % 'world'
+# print hw
+
+# x = "Hello World!"
+# print x.split("l")
+
+# drawer = ['documents', 'envelopes', 'pens']
+# print drawer[0]
+
+# x = [1,2,3,4,5]
+# x.append(12312321)
+# print x
+
+# for x in range (0, 10):
+#     print "looping ", x , " times"
+
+# my_list = [4, 'dog', 99, ['list', 'inside', 'another'], 'hello world!']
+# for x in my_list:
+#     print x
+
+# count = 0
+# while count < 5:
+#     print 'looping - ', count
+#     count += 1
+
+for val in "string":
+  if val == "n":
+    break
+  print val
