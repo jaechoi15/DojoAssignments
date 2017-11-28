634aa538e24895654a15825b34c2aafbda7c5db9
JaeChoi <jchoi625@gmail.com>
2017-11-28 00:13:45 -0500

    updated findingCharacters assignment

--- Python/FindingCharacters/findingCharacters.py
+++ Python/FindingCharacters/findingCharacters.py
@@ -3,3 +3,14 @@
 word_list = ['hello','world','my','name','is','Anna']
 char = 'o'
 
+def findCharacters(word_list, char):
+    new_word_list = []
+    for i in range(0, len(word_list)):
+        if word_list[i].find(char) != -1:
+            new_word_list.append(word_list[i])
+        else:
+            print "The specified character does not exist in position", i
+    
+    print new_word_list
+
+findCharacters(word_list, 'o')
