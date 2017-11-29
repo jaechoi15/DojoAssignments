b3b735e8611f739de740046a573a95d18c7cf38b
JaeChoi <jchoi625@gmail.com>
2017-11-29 00:03:16 -0500

    added scoresAndGrades assignment

--- Python/FunWithFunctions/funWithFunctions.py
+++ Python/FunWithFunctions/funWithFunctions.py
@@ -13,14 +13,14 @@ odd_even()
 # Multiply:
 # Create a function called 'multiply' that iterates through each value in a list (e.g. a = [2, 4, 10, 16]) and returns a list where each value has been multiplied by 5.
 
-a = [2, 4, 10, 16]
+# a = [2, 4, 10, 16]
 
 def multiply(a,num):
     for i in range (0,len(a)):
         a[i] *= num
     return a
 
-print multiply(a,5)
+# print multiply(a,5)
 
 # Hacker Challenge:
 # Write a function that takes the multiply function call as an argument. Your new function should return the multiplied list as a two-dimensional list. Each internal list should contain the 1's times the number in the original list.
---
+++ Python/ScoresAndGrades/scoresAndGrades.py
@@ -0,0 +1,23 @@
+# Write a function that generates ten scores between 60 and 100. Each time a score is generated, your function should display what the grade is for a particular score. Here is the grade table:
+# Score: 60 - 69; Grade - D
+# Score: 70 - 79; Grade - C
+# Score: 80 - 89; Grade - B
+# Score: 90 - 100; Grade - A
+
+from random import randint
+
+def scores_grades():
+    print "Score and Grades"
+    for i in range (0,10):
+        score = randint(60,100)
+        if score < 70:
+            print "Score:", score, "Your grade is D"
+        elif score > 69 and score < 80:
+            print "Score:", score, "Your grade is C"
+        elif score > 79 and score < 90:
+            print "Score:", score, "Your grade is B"
+        else:
+            print "Score:", score, "Your grade is A"
+    print "End of the program. Bye!"
+    
+scores_grades()
