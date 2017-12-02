baf511143c370cd8cdd5c35589263482e47ddc53
JaeChoi <jchoi625@gmail.com>
2017-12-02 17:46:45 -0500

    updated assignments

--- Python/Names/names.py
+++ Python/Names/names.py
@@ -1,11 +1,5 @@
 # Part 1
 
-# Create a program that outputs:
-# Michael Jordan
-# John Rosales
-# Mark Guillen
-# KB Tonel
-
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
@@ -13,7 +7,34 @@ students = [
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
 ]
 
-def names():
-    for i in 
+def studentNames(students):
+    for val in students:
+        print val['first_name'], val['last_name']
 
-names()
+studentNames(students)
+
+# Part II
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def studentsAndInstructors(users):
+    for job in users:
+        count = 0
+        print job
+        for val in users[job]:
+            count += 1
+            length = len(val['first_name']) + len(val['last_name'])
+            print str(count) + " " + val['first_name'].upper() + " " + val['last_name'].upper() + " - " + str(length)
+
+studentsAndInstructors(users)
--- WebFundamentals/Javascript/StudentsAndInstructors/studentsAndInstructors.js
+++ WebFundamentals/Javascript/StudentsAndInstructors/studentsAndInstructors.js
@@ -1,13 +1,5 @@
 // Given the following array of objects:
-
-// var students = [ 
-//      {first_name:  'Michael', last_name : 'Jordan'},
-//      {first_name : 'John', last_name : 'Rosales'},
-//      {first_name : 'Mark', last_name : 'Guillen'},
-//      {first_name : 'KB', last_name : 'Tonel'}
-// ]
 // Create a program that outputs:
-
 // Michael Jordan
 // John Rosales
 // Mark Guillen
@@ -29,21 +21,7 @@ function studentsAndInstructors()
 studentsAndInstructors();
    
 // Now, given the following dictionary:
-
-// var users = {
-//  'Students': [ 
-//      {first_name:  'Michael', last_name : 'Jordan'},
-//      {first_name : 'John', last_name : 'Rosales'},
-//      {first_name : 'Mark', last_name : 'Guillen'},
-//      {first_name : 'KB', last_name : 'Tonel'}
-//   ],
-//  'Instructors': [
-//      {first_name : 'Michael', last_name : 'Choi'},
-//      {first_name : 'Martin', last_name : 'Puryear'}
-//   ]
-//  }
 // Create a program that prints  the following format (including the number of characters in each combined name):
-
 // Students
 // 1 - MICHAEL JORDAN - 13
 // 2 - JOHN ROSALES - 11
@@ -52,7 +30,6 @@ studentsAndInstructors();
 // Instructors
 // 1 - MICHAEL CHOI - 11
 // 2 - MARTIN PURYEAR - 13
-
 
 var users = {
  'Students': [ 
