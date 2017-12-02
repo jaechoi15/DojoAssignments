a9f59c4f29020966057424e040421db8444b2bd0
JaeChoi <jchoi625@gmail.com>
2017-12-02 17:24:52 -0500

    added names.py

---
+++ Python/Names/names.py
@@ -0,0 +1,19 @@
+# Part 1
+
+# Create a program that outputs:
+# Michael Jordan
+# John Rosales
+# Mark Guillen
+# KB Tonel
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def names():
+    for i in 
+
+names()
