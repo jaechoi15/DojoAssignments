7e32af4ec49a8822a8bb04a6a0b582fbf26cc0e6
JaeChoi <jchoi625@gmail.com>
2017-12-02 17:11:03 -0500

    updated dictionary.py

--- Python/DictionaryBasics/dictionaryBasics.py
+++ Python/DictionaryBasics/dictionaryBasics.py
@@ -1,16 +1,9 @@
 # Create a dictionary containing some information about yourself. The keys should include name, age, country of birth, favorite language.
 
-# My name is Anna
-# My age is 101
-# My country of birth is The United States
-# My favorite language is Python
+my_dictionary = {"name": "Jae", "age": "208", "country of birth": "South Korea", "favorite language": "Python"}
 
-my_dictionary = {"name": "Jae", "age": "28", "country of birth": "South Korea", "favorite language": "Python"}
+def readDictionary(my_dictionary):
+    for key, value in my_dictionary.iteritems():
+        print "My " + key + " is " + value
 
-def readDictionary():
-    for val in my_dictionary.itervalues():
-        print val
-
-readDictionary()
-
-# print my_dictionary.get("name")
+readDictionary(my_dictionary)
