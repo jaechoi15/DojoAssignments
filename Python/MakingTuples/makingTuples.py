aefd6798a9f7b9e7ff2fbffb747bd6564cebb2a7
JaeChoi <jchoi625@gmail.com>
2017-12-02 18:01:05 -0500

    added makingTuples

---
+++ Python/MakingTuples/makingTuples.py
@@ -0,0 +1,12 @@
+# Write a function that takes in a dictionary and returns a list of tuples where the first tuple item is the key and the second is the value.
+
+# [("Speros", "(555) 555-5555"), ("Michael", "(999) 999-9999"), ("Jay", "(777) 777-7777")]
+
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+# def makeTuples():
+print my_dict['Speros']
