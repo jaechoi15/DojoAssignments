7970c8875a0533285f0c817e3d8bf096cb8c78c5
JaeChoi <jchoi625@gmail.com>
2017-12-03 21:47:58 -0500

    updated typeList.py

--- Python/TypeList/typeList.py
+++ Python/TypeList/typeList.py
@@ -5,14 +5,27 @@
 # At the end of your program print the string, the number and an analysis of what the list contains. 
 # If it contains only one type, print that type, otherwise, print 'mixed'.
 
-# Test Case 1
-#input
 mixed_type = ['magical unicorns',19,'hello',98.98,'world']
 integer_type = [2,3,1,7,4,12]
 string_type = ['magical','unicorns']
 
-
-#output
-# "The list you entered is of mixed type"
-# "String: magical unicorns hello world"
-# "Sum: 117.98"
+def listType(lst):
+    new_string = ''
+    total = 0
+    for val in lst:
+        if isinstance(val, int) or isinstance(val, float):
+            total += val
+        elif isinstance(val, str):
+            new_string += val
+    if new_string and total:
+        print "The list you entered is of mixed type."
+        print "String:", new_string
+        print "Sum:", total
+    elif total:
+        print "The list you entered is of integer type."
+        print "Sum:", total
+    else:
+        print "The list you entered is of string type."
+        print "String:", new_string
+        
+listType(string_type)
