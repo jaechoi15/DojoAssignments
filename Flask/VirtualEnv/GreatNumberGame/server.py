cc6b30f312c1feb89817e71de1f40f7b9c779f14
JaeChoi <jchoi625@gmail.com>
2017-12-21 00:41:42 -0500

    updated greatnumbergame

--- Flask/VirtualEnv/GreatNumberGame/server.py
+++ Flask/VirtualEnv/GreatNumberGame/server.py
@@ -1,31 +1,31 @@
 from flask import Flask, redirect, render_template, session, request
-
 import random
 
 app = Flask(__name__)
 app.secret_key = "asjiojasd"
 
-
-
 @app.route("/")
 def index():
+    if "mystery_num" not in session:
+        session["mystery_num"] = random.randrange(0,101)
+        print session["mystery_num"]
     return render_template("index.html")
-
-def number():
-    num = random.randrange(0,101)
-    return 
 
 @app.route("/guess", methods=["POST"])
 def guess():
-    random_num = number()
-    session["input"] = int(request.form["input"])
+    # session["input"] = int(request.form["input"])
+    if session["mystery_num"] == int(request.form["input"]):
+        session["result"] = "win"
+    elif session["mystery_num"] > int(request.form["input"]):
+        session["result"] = "low"
+    else:
+        session["result"] = "high"
+    return redirect("/")
 
-    if session["input"] == random_num:
-        session["status"] = "win"
-    elif session["input"] > random_num:
-        session["status"] = "high"
-    elif session["input"] < random_num:
-        session["status"] = "low"
+@app.route("/reset")
+def reset():
+    session.pop("mystery_num")
+    session.pop("result")
     return redirect("/")
 
 app.run(debug=True)
